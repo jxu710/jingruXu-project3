1199a39dc345783f557505e29a8936c24efe0b98
jingru <nicoxu1000@gmail.com>
2022-08-09 10:48:45 -0400

    added new h2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+ const x = 'true';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <h2>project branch is alive</h2>
+    {x}
+
+
     </div>
   );
 }
