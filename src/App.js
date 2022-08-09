be5250600f6859bdcedb51e173d8017ccc8823ea
jingru <nicoxu1000@gmail.com>
2022-08-09 19:03:52 -0400

    set up basic connection to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+import firebase from './firebase';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [pictures, setPictures] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const add = function(){
+    setCount(count + 1);
+  }
+
+  useEffect(()=>{
+
+    // holding database details
+    const database = getDatabase(firebase);
+
+    // variable that reference the specific location of our database
+    const dbRef = ref(database);
+
+    onValue(dbRef, (response) =>{
+      // console.log(response.val())
+
+      const newState = [];
+      const data = response.val();
+    
+      for (let key in data){
+         newState.push(data[key])
+        // console.log(newState)
+      }
+
+      setPictures(newState);
+    })
+
+    SetLoading(false)
+  
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the store!!!</h1>
+
+  
+      {
+       pictures.map( (pics) => {
+         return(
+          //  <li>
+           <img src={pics} onClick={add}/>
+          //  {/* </li> */}
+         )
+       })
+      }
+    
+       
+
+
+
+      {/* <i className="fa-solid fa-cart-shopping">
+
+        <p>{count}</p>
+      </i> */}
+
+
+      
     </div>
   );
+
 }
 
 export default App;
