46ac0e58f8c02b8ef3987f85e97bcbefe809b877
jingru <nicoxu1000@gmail.com>
2022-08-09 11:04:45 -0400

    added h3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import './App.css';
 
 function App() {
- const x = 'true';
   return (
     <div className="App">
       <h1>hello world</h1>
       <h2>project branch is alive</h2>
-    {x}
-
-
+      <h3>hello again</h3>
     </div>
   );
 }
