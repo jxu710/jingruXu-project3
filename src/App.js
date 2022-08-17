7a25d7af3b4b1048f4d24fe19466156d62962d09
Jingru Xu <jingruxu@Jingrus-Air.lan>
2022-08-17 15:43:45 -0400

    deleted file for soving conflicts

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <h2>project branch is alive</h2>
-      <h3>hello again</h3>
     </div>
   );
 }
